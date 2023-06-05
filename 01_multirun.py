7952969e160a3e301b867b0504bfeddbbc515c35
Kevin Villavicencio <xkdv.io@gmail.com>
2023-06-05 23:25:18 +0800

    Add draft multi-run script and results

--- 01_multirun.py
+++ 01_multirun.py
@@ -1,5 +1,6 @@
 import input_params as inputs
 import polars as pl
+import os
 
 import pythoncom
 pythoncom.CoInitialize()
@@ -36,9 +37,22 @@ manager = Automation3()
 
 myflowsheet = manager.CreateFlowsheet()
 
-# add compounds
+### DEFINE PARAMETERS
+components = {
+    "s1": ["Water", "Ethanol","Acetone"],
+    # "s2": ["C1", "C2","C3"]
+}
 
-cnames = ["Water", "Ethanol","Acetone"]
+paramBounds = {
+  "T": (300,350,2),
+  "P": (101325,202650,2),
+  "m": (300,400,2)
+}
+
+compositions = {
+  "s1": [[0.4,0.4,0.2]]
+}
+
 
 myflowsheet.AddCompound("Water")
 myflowsheet.AddCompound("Ethanol")
@@ -68,28 +82,37 @@ column.ConnectBottoms(bottoms)
 
 myflowsheet.NaturalLayout()
 
-### DEFINE PARAMETERS
-
-paramBounds = {
-  "T": (300,350,2),
-  "P": (101325,202650,2),
-  "m": (300,400,2)
-}
-
-compositions = {
-  "s1": [[0.4,0.4,0.2]]
-}
-
 (paramKeys, dataspace) = inputs.generate_dataspace(paramBounds, compositions)
+##  inputs = {list(paramKeys)[i]:[*(data[i] for data in dataspace)] for i in range(len(list(paramKeys)))}
+# create a dict with keys from the `paramKeys` list
+# - `paramKeys` is still a dict so `list(paramKeys)` is used
+# - we want to use the index of the key to get the corresponding value from the dataspace
+# - therefore, a for-loop from the range of the length is used
+# create a list composed of the values of the param at all rows
+# - for loop of the data space can be used
+# - from the individual dataspace item (data), the param value is taken using the index
+# - expand the resulting list using *args syntax
+inputs_df = pl.from_dict({list(paramKeys)[i]:[*(data[i] for data in dataspace)] for i in range(len(list(paramKeys)))})
+
+## NESTED SYNTAX 
+component_expr = []
+for stream in components.keys():
+  for i in range(len(components[stream])):
+    component_expr.append(pl.col(stream).list.get(i).alias(f"{stream}_{components[stream][i]}"))
+
+inputs_df = inputs_df.select(
+  [pl.exclude(pl.List(pl.Float64)),*component_expr,]
+)
 
 resultKeys = ["cduty", "rduty", "dtemp", "dflow", "btemp", "bflow" ]
-results = {k:[] for k in resultKeys}
+outputs = {k:[] for k in resultKeys}
 
-results.update({f"d_{k}":[] for k in cnames})
-results.update({f"b_{k}":[] for k in cnames})
+outputs.update({f"d_{k}":[] for k in components["s1"]})
+outputs.update({f"b_{k}":[] for k in components["s1"]})
 
 ### CALCULATION LOOP
 for paramValues in dataspace:
+  # Consider rewriting using polars dataframe directly
   data = dict(zip(paramKeys,paramValues))
 
   feed.SetOverallComposition(Array[float](data["s1"]))
@@ -122,26 +145,31 @@ for paramValues in dataspace:
 
   # get condenser and reboiler duties
 
-  results["cduty"].append(column.CondenserDuty)   # kW
-  results["rduty"].append(column.ReboilerDuty)    # kW
+  outputs["cduty"].append(column.CondenserDuty)   # kW
+  outputs["rduty"].append(column.ReboilerDuty)    # kW
 
-  results["dtemp"].append(dist.GetTemperature())  # K
-  results["dflow"].append(dist.GetMolarFlow())    # mol/s
-  results["btemp"].append(bottoms.GetTemperature())   # K
-  results["bflow"].append(bottoms.GetMolarFlow())     # mol/s
+  outputs["dtemp"].append(dist.GetTemperature())  # K
+  outputs["dflow"].append(dist.GetMolarFlow())    # mol/s
+  outputs["btemp"].append(bottoms.GetTemperature())   # K
+  outputs["bflow"].append(bottoms.GetMolarFlow())     # mol/s
 
   # product compositions
 
   dcomp = dist.GetOverallComposition()
-  for i in range(len(cnames)):
-    results[f"d_{cnames[i]}"].append(dcomp[i])
+  for i in range(len(components['s1'])):
+    outputs[f"d_{components['s1'][i]}"].append(dcomp[i])
 
   bcomp = bottoms.GetOverallComposition()
-  for i in range(len(cnames)):
-    results[f"b_{cnames[i]}"].append(dcomp[i])
+  for i in range(len(components['s1'])):
+    outputs[f"b_{components['s1'][i]}"].append(dcomp[i])
 
-# save file
-print("SAVING")
-df = pl.from_dict(results)
-print(df)
-df.write_csv("new.csv", separator=",")
+# save files
+
+
+outputs_df = pl.from_dict(outputs)
+print(inputs_df.head(5))
+print(outputs_df.head(5))
+# df.write_csv("new.csv", separator=",")
+rootdir = os.path.dirname(__file__)
+inputs_df.write_csv(os.path.join(rootdir, "outputs/inputs.csv"), separator=",")
+outputs_df.write_csv(os.path.join(rootdir, "outputs/outputs.csv"), separator=",")
